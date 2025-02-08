b3b6818eee8a8e422d7f95e7fc563dc530f917f5
Rifana20 <rifanasherin547@gmail.com>
2025-02-08 15:22:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.markdown(
     """
     <style>
     h1 {
-        font-size: 90px !important;  /* Forced 100px Size */
+        font-size: 60px !important;  /* Forced 100px Size */
         text-align: center;
         color: #ffffff;
         font-weight: bold;
@@ -55,7 +55,7 @@ st.markdown(
     }
     
     h2 {
-        font-size: 50px !important;  /* Subtitle Larger */
+        font-size: 30px !important;  /* Subtitle Larger */
         text-align: center;
         color: #ffffff;
         font-weight: bold;
